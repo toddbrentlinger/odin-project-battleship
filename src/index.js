68e65a25e2cd26b8f448e26586638cb1b8771bae
toddbrentlinger <toddbrentlinger@gmail.com>
2023-08-19 18:58:19 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = 'Hello World';
+
+    return element;
+}
+
+document.body.appendChild(component());
