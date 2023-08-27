c5ba4ce0e27fc7ea16183a84c1e9ed726d7ba071
toddbrentlinger <toddbrentlinger@gmail.com>
2023-08-26 18:52:30 -0700

    Add logo images and add one to header

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,11 +1,13 @@
 import BaseComponent from "./baseComponent";
 import { createElement } from "../utilities";
+import BattleshipLogoSVG from "../images/battleship-drawing.svg";
 
 class HeaderComponent extends BaseComponent {
     render() {
         this.initializeRender('header');
 
-        this.element.appendChild(
+        this.element.append(
+            createElement('img', {'class': 'logo', src: BattleshipLogoSVG, alt: 'Battleship logo'}),
             createElement('h1', {}, this.props.title)
         );
 
