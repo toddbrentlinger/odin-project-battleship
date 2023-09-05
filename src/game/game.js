b3e1c9bd56171436f54115886f226af4a1036919
toddbrentlinger <toddbrentlinger@gmail.com>
2023-09-04 18:35:43 -0700

    Add attack methods in game module
    Adds player attack method that accepts coordinates to attack.
    Adds computer attack method that makes random attack.
    Toggles isPlayersTurn flag in each method to prevent player or computer
    taking second turn before other player.
    Adds boardSize property that is passed into Gameboard instances.

--- src/game/game.js
+++ src/game/game.js
@@ -13,9 +13,11 @@ class GameState {
 const Game = (() => {
     let state = GameState.Preparation;
     let isPlayersTurn = true;
+
+    const boardSize = 10;
     const gameboards = {
-        'player': Gameboard(),
-        'computer': Gameboard(),
+        'player': Gameboard(boardSize),
+        'computer': Gameboard(boardSize),
     };
 
     const init = () => {
@@ -42,11 +44,61 @@ const Game = (() => {
         // Display winner and end message
     };
 
+    const enterPlayerAttack = (x, y) => {
+        // Return if NOT Playing state OR NOT player's turn
+        if (state !== GameState.Playing || !isPlayersTurn) { return; }
+
+        console.log('Player attacks!');
+
+        // Send attack coordinates to computer's board, throwing error if NOT valid
+        gameboards.computer.recieveAttack(x,y);
+
+        // Check if computer has lost
+        if (gameboards.computer.areAllShipsSunk()) {
+            end();
+        }
+
+        // Set isPlayersTurn flag to false to prevent another player attack before computer can make an attack
+        isPlayersTurn = false;
+    };
+
+    const makeComputerAttack = () => {
+        // Return if NOT Playing state OR NOT computer's turn
+        if (state !== GameState.Playing || isPlayersTurn) { return; }
+
+        console.log('Computer attacks!');
+
+        // Make computer attack
+        let validAttack = false;
+        while (!validAttack) {
+            try {
+                gameboards.player.recieveAttack(
+                    Math.floor(Math.random() * boardSize),
+                    Math.floor(Math.random() * boardSize)
+                );
+                validAttack = true;
+            } catch(e) {
+                
+            }
+        }
+
+        // Check if player has lost
+        if (gameboards.player.areAllShipsSunk()) {
+            end();
+        }
+
+        // Set isPlayersTurn flag so Player can make next attack
+        isPlayersTurn = true;
+    };
+
     return {
         createNew,
         play,
         get playerBoard() { return gameboards.player; },
         get computerBoard() { return gameboards.computer; },
+        get isPlayersTurn() { return isPlayersTurn; },
+        enterPlayerAttack,
+        makeComputerAttack,
     };
 })();
 
