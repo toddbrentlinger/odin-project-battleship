f6a362b760805b31d9e57b0dc08df474214a8095
toddbrentlinger <toddbrentlinger@gmail.com>
2023-09-09 19:10:50 -0700

    Continue GameboardComponent class
    Adds explosion gif that's displayed in board node when ship is hit.
    Adds water splash gif that's displayed in board node when no ship is hit.
    Adds method to render board nodes using separate component class
    for single node.
    Starts method to render ships on board.
    Renders table depending on Gameboard sent as prop.
    Updates misc styling.

--- src/components/gameboardComponent.js
+++ src/components/gameboardComponent.js
@@ -1,22 +1,63 @@
 import { createElement } from "../utilities";
 import BaseComponent from "./baseComponent";
+import ExplosionGIF from "../gifs/explosion.gif";
+import SplashGIF from "../gifs/water-splash.gif";
 
 class GameboardComponent extends BaseComponent {
+    constructor(props) {
+        super(props);
+        this.boardTableElement = null;
+        this.shipsTableElement = null;
+    }
+
     render() {
+        const { board } = this.props;
+
         this.initializeRender();
 
         this.element.classList.add('gameboard-container');
 
-        const tableElement = this.element.appendChild(
-            createElement('table', {'class': 'gameboard'})
-        );
+        this.element.appendChild(this.renderTable());
+
+        //this.element.appendChild(this.renderShips());
+
+        return this.element;
+    }
+
+    renderShips() {
+        const { board } = this.props;
+        let shipElement;
+
+        this.shipsTableElement = createElement('table', {'class': 'ships-table'});
+        this.shipsTableElement.style = `grid-template-rows: repeat(${board.size + 1}, 1fr);`
+
+        board.ships.forEach((ship) => {
+            shipElement = this.shipsTableElement.appendChild(
+                createElement('tr', {'class': 'ship'})
+            );
+            shipElement.style = `grid-template-columns: repeat(${board.size + 1}, 1fr);`;
+
+            for (let i = 0; i < ship.length; i++) {
+                shipElement.appendChild(
+                    createElement('td', {}, ship.name[0])
+                );
+            }
+        });
+
+        return this.shipsTableElement;
+    }
+
+    renderTable() {
+        const { board } = this.props;
+
+        this.boardTableElement = createElement('table', {'class': 'gameboard'});
 
         let content;
         let dataElement;
-        for (let i = 0; i < 11; i++) {
-            let rowElement = tableElement.appendChild(createElement('tr', {}));
+        for (let i = 0; i < this.props.board.size + 1; i++) {
+            let rowElement = this.boardTableElement.appendChild(createElement('tr', {}));
 
-            for (let j = 0; j < 11; j++) {
+            for (let j = 0; j < this.props.board.size + 1; j++) {
                 // First row
                 if (i === 0) {
                     // If first column
@@ -42,18 +83,64 @@ class GameboardComponent extends BaseComponent {
                     }
                     // Else NOT first column 
                     else {
-                        dataElement = rowElement.appendChild(createElement('td', {}, 
-                            createElement('span', {}, '\u00A0')
-                        ));
-
-                        dataElement.dataset.x = i - 1;
-                        dataElement.dataset.y = j - 1;
+                        rowElement.appendChild(
+                            new GameboardNodeComponent({
+                                children: [createElement('span', {}, '\u00A0'),],
+                                x: i - 1,
+                                y: j - 1,
+                            }).render()
+                        );
                     }
                 }
             }
         }
 
+        return this.boardTableElement;
+    }
+}
+
+class GameboardNodeComponent extends BaseComponent {
+    handleClick() {
+        const { x, y } = this.props;
+
+        console.log(`Clicked x: ${x} - y: ${y}`);
+        
+        Math.floor(Math.random() * 2) ? this.setMiss() : this.setAttack();
+    }
+
+    render() {
+        const { children, x, y } = this.props;
+
+        this.initializeRender('td');
+
+        this.element.classList.add('gameboard-node');
+
+        this.element.append(...children);
+
+        this.element.dataset.x = x;
+        this.element.dataset.y = y;
+
+        this.element.addEventListener('click', this.handleClick.bind(this));
+
         return this.element;
+    }
+
+    setAttack() {
+        this.element.style.backgroundImage = `url(${ExplosionGIF})`;
+        this.element.style.backgroundColor = 'inherit';
+        setTimeout(() => {
+            this.element.style.backgroundImage = null;
+            this.element.style.backgroundColor = 'red';
+        }, 1700);
+    }
+
+    setMiss() {
+        this.element.style.backgroundImage = `url(${SplashGIF})`;
+        this.element.style.backgroundColor = 'inherit';
+        setTimeout(() => {
+            this.element.style.backgroundImage = null;
+            this.element.style.backgroundColor = 'blue';
+        }, 1500);
     }
 }
 
